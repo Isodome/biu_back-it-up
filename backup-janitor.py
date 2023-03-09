7bbd6a35547df60d86e1c492e74f2d66d72ffd3d
Dominic Rausch <328764+Isodome@users.noreply.github.com>
2023-03-09 14:09:44 +0100

    Add algorithm to determine backups to be deleted.

--- backup-janitor.py
+++ backup-janitor.py
@@ -21,21 +21,45 @@ import os
 import argparse
 import re
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from collections import namedtuple
 
-parser = argparse.ArgumentParser(
-                    prog = 'backup-janitor',
-                    description = 'What the program does',
-                    epilog = 'Text at the bottom of help')
-parser.add_argument('path')  
-args = parser.parse_args()
+
+def parse_backup_plan(arg):
+    plan = {}
+    intervals = arg.split(',')
+
+    def parse_interval(interval):
+        (k,v) = interval.split(":")
+        return (timedelta(hours = int(k)), int(v))
+
+    plan = {parse_interval(k) for k in intervals}
+    return plan if len(plan)>0 else None
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+                        prog = 'backup-janitor',
+                        description = 'What the program does',
+                        epilog = 'Text at the bottom of help')
+    parser.add_argument('path')  
+    parser.add_argument('-p', '--plan', required = True, type=parse_backup_plan)
+    return parser.parse_args()
 
 
 class Options(object):
     follow_symlinks = False
+    plan = {}
 
-Backup = namedtuple("Backup", "path datetime")
+class Backup:
+    def __init__(self, path, creation_time):
+        self.creation_time = creation_time
+        self.path = path
+ 
+    path = None
+    creation_time = None
+    should_keep = False
 
 
 def parse_datetime(datetime_str):
@@ -61,15 +85,34 @@ def list_backups(path):
     for dir in dirs:
         try:
             backup_time =  parse_datetime(dir.name)
-            backups.append(Backup(path = dir, datetime=backup_time))
+            backups.append(Backup(path = dir, creation_time=backup_time))
         except ValueError:
             print(f'Ignoring "{dir.name}" since the name can\'t be parsed to date/time.')
             continue
+    def by_creation_time(b):
+        return b.creation_time
+    backups.sort(key=by_creation_time)
     return backups
 
 
+def mark_save(interval, iterations, backups):
+    now = datetime.now()
+    for i in range(iterations+1):
+        backup_to_keep = now - i * interval
+        best_match = min(enumerate(backups), key=lambda b:abs((b[1].creation_time - backup_to_keep).total_seconds()))
+        backups[best_match[0]].should_keep = True
+
 def main():
-    print (list_backups(args.path))
+    args = parse_args()
+    print (args.plan)
+    backups = list_backups(args.path)
+
+    for (interval, iterations) in args.plan:
+        mark_save(interval, iterations, backups)
+
+    for b in backups:
+        op = "keep" if b.should_keep else "rm  "
+        print(f'{op} {b.path}')
 
 if __name__ == '__main__':
     main()
